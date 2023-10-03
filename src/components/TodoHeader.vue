827795156c3761327c5f9899c1714d3c883dd5d2
artem <artemfr@hotmail.com>
2023-10-03 18:02:00 +0200

    Added home and about

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="../assets/Vue_Logo_Black.png" alt="" srcset="">
+                <h1>Vue Todos</h1>
+            </div>
+            <ul class="nav-routes">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
