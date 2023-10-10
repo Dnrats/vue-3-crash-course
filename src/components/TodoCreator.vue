50f0c67ffbc644428b9773515b0955e13314fe7c
artem <artemfr@hotmail.com>
2023-10-10 15:20:26 +0200

    Added error message. Also when youadd a new todo, the messages disappears from the input holder

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,14 +1,33 @@
 <template>
     <div class="input-wrap">
-        <input type="text" v-model="todo">
-        <button @click="">Create</button>
+        <input type="text" v-model="todoState.todo"/>
+        <button @click="createTodo()">Create</button>
     </div>
+    <p class="err-msg">{{ todoState.errMsg }}</p>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {reactive} from "vue";
 
-const todo = ref("Testing");
+const emit = defineEmits (["create-todo"]);
+
+const todoState = reactive({
+  todo: "",
+  invalid: null,
+  errMsg: "",
+});
+
+const createTodo = ( ) => {
+  todoState.invalid = null;
+  if (todoState.todo !== "") {
+    emit("create-todo", todoState.todo);
+  todoState.todo = "";
+  return; 
+  }
+  todoState.invalid = true;
+  todoState.errMsg = "Todo value cannot be empty";
+}
+
 </script>
 
 <style lang="scss" scoped>
